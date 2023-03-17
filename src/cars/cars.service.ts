540d191517995f6260235fe48c210e9e817fee8b
migCortes <migcortess@gmail.com>
2023-03-16 23:31:28 -0600

    Initial Commit for TestCarAPI

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+    private cars = [
+    {
+        id: 1,
+        brand: 'Toyota',
+        model: 'Corolla',
+    },
+    {
+        id: 2,
+        brand: 'Honda',
+        model: 'Seat',
+    },
+    {
+        id: 3,
+        brand: 'Jeep',
+        model: 'Cherokee',
+    },
+]
+
+public findAll() {
+    return this.cars;
+}
+
+public findOneById(id: number){
+    const car = this.cars.find(car => car.id === id);
+    if(!car) throw new NotFoundException(`Car with id '${id}' not found.`);
+    return car;
+}
+
+}
