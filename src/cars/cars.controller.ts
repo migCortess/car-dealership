98d4c2b3191d607816a43e66755b79009571e36b
migCortes <migcortess@gmail.com>
2023-03-19 02:06:00 -0600

    UUID code

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -11,7 +11,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
     return this.carsService.findOneById(id);
   }
 
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,20 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CarsService {
-    private cars = [
+    private cars: Car[] = [
     {
-        id: 1,
+        id: uuid(),
         brand: 'Toyota',
         model: 'Corolla',
     },
     {
-        id: 2,
+        id: uuid(),
         brand: 'Honda',
         model: 'Seat',
     },
     {
-        id: 3,
+        id: uuid(),
         brand: 'Jeep',
         model: 'Cherokee',
     },
@@ -24,7 +26,7 @@ public findAll() {
     return this.cars;
 }
 
-public findOneById(id: number){
+public findOneById(id: string){
     const car = this.cars.find(car => car.id === id);
     if(!car) throw new NotFoundException(`Car with id '${id}' not found.`);
     return car;
