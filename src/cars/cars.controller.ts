b7f27983e5afa0fbe5fd31632c3f071c7c88cd0b
migCortes <migcortess@gmail.com>
2023-03-19 16:27:19 -0600

    POST Method with validations

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -16,8 +17,8 @@ export class CarsController {
   }
 
   @Post()
-  createCar(@Body() body: any) {
-    return body;
+  createCar(@Body() CreateCarDto: CreateCarDto) {
+    return this.carsService.create(CreateCarDto);
   }
 
   @Patch(':id')
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -29,6 +30,17 @@ public findAll() {
 public findOneById(id: string){
     const car = this.cars.find(car => car.id === id);
     if(!car) throw new NotFoundException(`Car with id '${id}' not found.`);
+    return car;
+}
+
+public create(CreateCarDto: CreateCarDto){
+    const car: Car = {
+        id: uuid(),
+        ...CreateCarDto
+    }
+
+    this.cars.push(car);
+
     return car;
 }
 
